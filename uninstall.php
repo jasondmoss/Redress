8ec9776528eb41f3446492998db34a64ab4b6442
Jason D. Moss <jason@jdmlabs.com>
2017-03-27 08:27:12 -0300

    Add uninstall file for removing optons from the database.

---
+++ uninstall.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Redress.
+ *
+ * @package    WordPress
+ * @subpackage Redress
+ * @author     Jason D. Moss <jason@jdmlabs.com>
+ * @copyright  2017 Jason D. Moss. All rights freely given.
+ * @license    https://github.com/jasondmoss/redress/blob/master/LICENSE.md [WTFPL License]
+ * @link       https://github.com/jasondmoss/redress/
+ */
+
+
+/**
+ * If {uninstall} is not called from WordPress, exit.
+ */
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit();
+}
+
+/**
+ * Define options that need to be removed upon plug-in uninstall.
+ *
+ * @var array
+ */
+// $options = [
+//     'option_name'
+// ];
+
+/**
+ * Deelet each option from the database.
+ */
+// array_walk($options, function ($option, $key) {
+//     delete_option($option);
+// });
+
+/* <> */
